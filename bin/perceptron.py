d5cc670c1283d83800b7c18b5e4ef87520acfa8f
nmcphers128 <nhm5as@virginia.edu>
2023-10-16 02:20:11 +0000

    committing some changes to for the TESTs lab

--- bin/perceptron.py
+++ bin/perceptron.py
@@ -5,7 +5,7 @@ class Perceptron:
     ''' This is the class docstring '''
     def __init__(self):
         ''' this is a docstring '''
-        self._weights = 0
+        self._weights = 0.0
 
     def train(self, inputs, labels):
         '''This is the docstring for the train func'''
@@ -14,8 +14,8 @@ class Perceptron:
         for _ in range(5000):
             for myinput, label in zip(dummied_inputs, labels):
                 label_delta = (label - self.predict(myinput))
-            for index, mycount in enumerate(inputs):
-                self._weights[index] += .1 * mycount * label_delta
+                for index, mycount in enumerate(myinput):
+                    self._weights[index] += .1 * mycount * label_delta
 
     def predict(self, myinput):
         '''This is the docstring for the predict func'''
